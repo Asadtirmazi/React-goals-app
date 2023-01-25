b6086a486e28359b3813c9c3f35a30059ae03e2d
Asad Tirmazi <syedasadse@gmail.com>
2023-01-25 23:16:31 +0500

    project complete

---
+++ src/components/Progress.js
@@ -0,0 +1,11 @@
+import './Progress.css'
+
+function Progress(){
+return(
+    <div className="progress">
+        this project is DONE
+        <div className='a'> by Syed Asad Tirmazi</div>
+    </div>
+)
+}
+export default Progress
